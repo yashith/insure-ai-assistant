fd34d040e0aa9087735cdeb20b5f7684cda0777c
Yashith <yashith98@gmail.com>
2025-08-17 13:51:53 +0530

    Initial setup

---
+++ backend/nest-backend/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {ConfigService} from "@nestjs/config";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const config = new DocumentBuilder()
+      .setTitle("Insurance AI backend")
+      .setDescription('API documentation with executable Swagger UI')
+      .setVersion('1.0')
+      .addBearerAuth()  // For JWT
+      .build();
+  const  document = SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup("api-docs",app,document);
+  const port = configService.get<number>('PORT') ?? 3000;
+  await app.listen(port);
+
+}
+bootstrap();
